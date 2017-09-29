80b5a0689848dd7753d23bbd8add92c7852e8f68
tanglei02 <tanglei02@baidu.com>
2017-09-29 16:57:18 +0800

    update html

--- sw-register/sw-1.js
+++ sw-register/sw-1.js
@@ -1,10 +1,12 @@
 var log = localStorage.getItem('log');
+
 try {
     log = JSON.parse(log);
 }
 catch (e) {
     log = [];
 }
+
 log.push('in sw-1');
 log = JSON.stringify(log);
 localStorage.setItem('log', log);
--- sw-register/sw-2.js
+++ sw-register/sw-2.js
@@ -1,10 +1,12 @@
 var log = localStorage.getItem('log');
+
 try {
     log = JSON.parse(log);
 }
 catch (e) {
     log = [];
 }
+
 log.push('in sw-2');
 log = JSON.stringify(log);
 localStorage.setItem('log', log);
